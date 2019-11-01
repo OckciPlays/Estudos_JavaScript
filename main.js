dc433a227a6dd87ed86017329edb42d08d7257dc
Gustavo Rodrigues <gustavorodrigues12366@gmail.com>
2019-11-01 16:20:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var nome = "Gustavo Rodrigues";
+var idade = 19;
+var frase = "Gustavo é foda";
+alert("bem Vindo : " + nome);
+alert("Idade : " + idade);
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Gustavo","JavaScript").toUpperCase());
