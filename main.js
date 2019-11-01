c235dbccbea3f3beb6290a3f6e39a31b9c85041a
Gustavo Rodrigues <gustavorodrigues12366@gmail.com>
2019-11-01 17:03:37 -0300

    commit 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var nome = "Gustavo Rodrigues";
+/*var nome = "Gustavo Rodrigues";
 var idade = 19;
 var frase = "Gustavo é foda";
 alert("bem Vindo : " + nome);
@@ -6,3 +6,15 @@ alert("Idade : " + idade);
 console.log(nome);
 console.log(idade);
 console.log(frase.replace("Gustavo","JavaScript").toUpperCase());
+*/
+
+//var lista =["maça","pera","uva"];
+//lista.push("laranja"); adiciona um elemento ao final da lista
+//lista.pop(); excluir o ultimo elemento
+//console.log(lista.length); tamanho da lista
+//console.log(lista.reverse()); inverte a ordem da lista
+//console.log(lista.toString()); transforma em string
+//console.log(lista.toString()[0]); usar indice para pegar as letras
+//console.log(lista.join(" - ")); transfoma em string porem separa os elementos com o caractere que vc colocar
+var fruta = [{nome:"maça", cor:"vermelha"},{nome:"maça", cor:"vermelha"}]
+console.log(fruta);
